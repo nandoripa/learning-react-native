c50707d402188fa1eb97df7aa93af7f6e2e125c2
nandoripa <nandoripa@gmail.com>
2017-03-07 00:05:07 +0100

    Added Firebase authentication config

--- src/views/loginView.js
+++ src/views/loginView.js
@@ -12,13 +12,47 @@ import {
 } from 'react-native';
 
 import {Actions} from 'react-native-router-flux';
+import * as firebase from 'firebase';
 
 import FBSDK, {
   LoginButton,
   AccessToken
 } from 'react-native-fbsdk'
 
+const config = {
+  apiKey: "AIzaSyDlq2wBi_83mL3f0VJMZjx3Kv8HTwspfis",
+  authDomain: "platzimusic-1b87a.firebaseapp.com",
+  databaseURL: "https://platzimusic-1b87a.firebaseio.com",
+  storageBucket: "platzimusic-1b87a.appspot.com",
+  messagingSenderId: "338115614310"
+};
+firebase.initializeApp(config);
+
+const {FacebookAuthProvider} = firebase.auth;
+const firebaseAuth = firebase.auth();
+
 export default class LoginView extends Component {
+
+  state = {
+    credentials: null
+  }
+
+  componentWillMount() {
+    this.authenticateUser();
+  }
+
+  authenticateUser = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      const {accessToken} = data;
+      const credential = FacebookAuthProvider.credential(accessToken);
+
+      firebaseAuth.signInWithCredential(credential).then((credentials) => {
+        Actions.root();
+      }, function(error) {
+        console.log("Sign In Error", error);
+      });
+    })
+  }
 
   handleLoginFinished = (error, result) => {
     if (error) {
@@ -26,9 +60,7 @@ export default class LoginView extends Component {
     } else if (result.isCancelled) {
       alert("login is cancelled.");
     } else {
-      AccessToken.getCurrentAccessToken().then(
-        Actions.root()
-      )
+      this.authenticateUser();
     }
   };
 
